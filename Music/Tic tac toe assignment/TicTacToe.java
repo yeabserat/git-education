be1a6941dcacebdb89d233910d7b8ea04e6266a3
yeabserat <yeabsera.seyoum@bitscollege.edu.et>
2021-11-30 08:34:11 +0300

    tic tac toe done

--- Music/Tic tac toe assignment/TicTacToe.java
+++ Music/Tic tac toe assignment/TicTacToe.java
@@ -6,67 +6,67 @@ public class TicTacToe {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 		
-		char[][] board = {{' ', ' ', ' '},
+		char[][] display = {{' ', ' ', ' '},
 				 	      {' ', ' ', ' '}, 
 				 	      {' ', ' ', ' '}};
 		
-		Board(board);
+		Board(display);
 		
 		while (true) {
-			playerTurn(board, scanner);
-			if (roundEnd(board)){
+			playerTurn(display, scanner);
+			if (roundEnd(display)){
 				break;
 			}
-			Board(board);
+			Board(display);
 			
-			computerTurn(board);
-			if (roundEnd(board)){
+			computerTurn(display);
+			if (roundEnd(display)){
 				break;
 			}
-			Board(board);
+			Board(display);
 		}
 		scanner.close();
 	}
 
 
-	private static boolean roundEnd(char[][] board) {
+	private static boolean roundEnd(char[][] display) {
 		
-		if (Won(board, 'X')) {	
-			Board(board);
+		if (Won(display, 'X')) {	
+			Board(display);
 			System.out.println("Player wins!");
 			return true;
 		}
 		
-		if (Won(board, 'O')) {	
-			Board(board);
+		if (Won(display, 'O')) {	
+			Board(display);
 			System.out.println("Computer wins!");
 			return true;
 		}
 		
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board[i].length; j++) {
-				if (board[i][j] == ' ') {
+		for (int i = 0; i < display.length; i++) {
+			for (int j = 0; j < display[i].length; j++) {
+				if (display[i][j] == ' ') {
 					return false;
 				}
 			}
 		}
-		Board(board);
+		Board(display);
 		System.out.println("TIE!");
 		return true;
 	}
 
 
-	private static boolean Won(char[][] board, char symbol) {
-		if ((board[0][0] == symbol && board [0][1] == symbol && board [0][2] == symbol) ||
-			(board[1][0] == symbol && board [1][1] == symbol && board [1][2] == symbol) ||
-			(board[2][0] == symbol && board [2][1] == symbol && board [2][2] == symbol) ||
+	private static boolean Won(char[][] display, char symbol) {
+		if ((display[0][0] == symbol && display [0][1] == symbol && display [0][2] == symbol) ||
+			(display[1][0] == symbol && display [1][1] == symbol && display [1][2] == symbol) ||
+			(display[2][0] == symbol && display [2][1] == symbol && display [2][2] == symbol) ||
 			
-			(board[0][0] == symbol && board [1][0] == symbol && board [2][0] == symbol) ||
-			(board[0][1] == symbol && board [1][1] == symbol && board [2][1] == symbol) ||
-			(board[0][2] == symbol && board [1][2] == symbol && board [2][2] == symbol) ||
+			(display[0][0] == symbol && display [1][0] == symbol && display [2][0] == symbol) ||
+			(display[0][1] == symbol && display [1][1] == symbol && display [2][1] == symbol) ||
+			(display[0][2] == symbol && display [1][2] == symbol && display [2][2] == symbol) ||
 			
-			(board[0][0] == symbol && board [1][1] == symbol && board [2][2] == symbol) ||
-			(board[0][2] == symbol && board [1][1] == symbol && board [2][0] == symbol) ) {
+			(display[0][0] == symbol && display [1][1] == symbol && display [2][2] == symbol) ||
+			(display[0][2] == symbol && display [1][1] == symbol && display [2][0] == symbol) ) {
 			return true;
 		}
 		return false;
@@ -87,74 +87,74 @@ public class TicTacToe {
 	}
 
 
-	private static boolean valid (char[][] board, String position) {
+	private static boolean valid (char[][] display, String position) {
 		switch(position) {
 			case "1":
-				return (board[0][0] == ' ');
+				return (display[0][0] == ' ');
 			case "2":
-				return (board[0][1] == ' ');
+				return (display[0][1] == ' ');
 			case "3":
-				return (board[0][2] == ' ');
+				return (display[0][2] == ' ');
 			case "4":
-				return (board[1][0] == ' ');
+				return (display[1][0] == ' ');
 			case "5":
-				return (board[1][1] == ' ');
+				return (display[1][1] == ' ');
 			case "6":
-				return (board[1][2] == ' ');
+				return (display[1][2] == ' ');
 			case "7":
-				return (board[2][0] == ' ');
+				return (display[2][0] == ' ');
 			case "8":
-				return (board[2][1] == ' ');
+				return (display[2][1] == ' ');
 			case "9":
-				return (board[2][2] == ' ');
+				return (display[2][2] == ' ');
 			default:
 				return false;
 		}
 	}
 
-	private static void playerTurn(char[][] board, Scanner scanner) {
+	private static void playerTurn(char[][] display, Scanner scanner) {
 		String userInput;
 		while (true) {
 			System.out.println("Enter a number (1-9)");
 			userInput = scanner.nextLine();
-			if (valid(board, userInput)){
+			if (valid(display, userInput)){
 				break;
 			} else {
 				System.out.println(userInput + "INVALID!");
 			}
 		}
-		Move(board, userInput, 'X');
+		Move(display, userInput, 'X');
 	}
 
 
-	private static void Move(char[][] board, String position, char symbol) {
+	private static void Move(char[][] display, String position, char symbol) {
 		switch(position) {
 			case "1":
-				board[0][0] = symbol;
+				display[0][0] = symbol;
 				break;
 			case "2":
-				board[0][1] = symbol;
+				display[0][1] = symbol;
 				break;
 			case "3":
-				board[0][2] = symbol;
+				display[0][2] = symbol;
 				break;
 			case "4":
-				board[1][0] = symbol;
+				display[1][0] = symbol;
 				break;
 			case "5":
-				board[1][1] = symbol;
+				display[1][1] = symbol;
 				break;
 			case "6":
-				board[1][2] = symbol;
+				display[1][2] = symbol;
 				break;
 			case "7":
-				board[2][0] = symbol;
+				display[2][0] = symbol;
 				break;
 			case "8":
-				board[2][1] = symbol;
+				display[2][1] = symbol;
 				break;
 			case "9":
-				board[2][2] = symbol;
+				display[2][2] = symbol;
 				break;
 			default:
 				System.out.println();
@@ -164,12 +164,12 @@ public class TicTacToe {
 	
 	
 	
-	private static void Board(char[][] board) {
-		System.out.println(board[0][0] + "|" +  board[0][1] + "|" +  board[0][2] );
+	private static void Board(char[][] display) {
+		System.out.println(display[0][0] + "|" +  display[0][1] + "|" +  display[0][2] );
 		System.out.println("-----");
-		System.out.println(board[1][0] + "|" +  board[1][1] + "|" +  board[1][2] );
+		System.out.println(display[1][0] + "|" +  display[1][1] + "|" +  display[1][2] );
 		System.out.println("-----");
-		System.out.println(board[2][0] + "|" +  board[2][1] + "|" +  board[2][2] );
+		System.out.println(display[2][0] + "|" +  display[2][1] + "|" +  display[2][2] );
 	}
 	
 }
